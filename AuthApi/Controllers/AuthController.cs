abd477d73c168ec6641f2276ffd61759a73a4c47
fredrik <freedrik92@hotmail.com>
2025-06-03 17:57:35 +0200

    updated JWT

--- AuthApi/Controllers/AuthController.cs
+++ AuthApi/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using AuthApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace AuthApi.Controllers
 {
@@ -9,24 +13,45 @@ namespace AuthApi.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
-        public AuthController(IHttpClientFactory httpClientFactory)
+        private readonly IConfiguration _configuration;
+        public AuthController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginForm form)
         {
             var client = _httpClientFactory.CreateClient();
-            var response = await client.PostAsJsonAsync("https://localhost:7093/api/account/verify", form);
+            var response = await client.PostAsJsonAsync("https://accountprovider.azurewebsites.net/api/account/verify", form);
 
-            if (response.IsSuccessStatusCode)
+            if (!response.IsSuccessStatusCode)
+                return Unauthorized("Invalid credentials");
+
+            var token = GenerateJwtToken(form.Email);
+            return Ok(new { token });
+        }
+
+        private string GenerateJwtToken(string email)
+        {
+            var claims = new[]
             {
-                return Ok("Login successful"); // add token here later
-            }
+                new Claim(ClaimTypes.Email, email)
+            };
 
-            return Unauthorized("Invalid credentils");
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
